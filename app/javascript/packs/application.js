1b2273b26f1ba1d4c8b8e13e2df68032abfa2e52
Sean Doyle <sean.p.doyle24@gmail.com>
2020-10-01 00:07:19 -0400

    [GENERATED]: Install ActiveStorage and ActionText
    This commit installs and configures both
    [`active_storage`][active_storage] and [`action_text`][action_text].
    
    This contents were generated by executing the following commands:
    
    ```bash
    bin/rails active_storage:install
    bin/rails action_text:install
    bin/rails action_text:install:migrations
    bin/rails db:migrate
    bin/setup
    ```
    
    [action_text]: https://edgeguides.rubyonrails.org/action_text_overview.html#installation
    [active_storage]: https://edgeguides.rubyonrails.org/active_storage_overview.html

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import "trix"
+import "@rails/actiontext"
 import "channels"
 
 Rails.start()
