428b91ecd077a89c75c83530fbff1609951565ea
Sean Doyle <sean.p.doyle24@gmail.com>
2020-10-04 03:02:56 -0400

    Abstract Validation Message ID generation
    Push the concept of `ValidationMessageFormBuilder#field_id` further, and
    declare `validation_message_id` as a specialized version.
    
    For fields that _have no errors_ for the attribute, return `nil`.
    
    For fields that _have at least one error_ for the attribute, return the
    DOM id.

--- lib/validation_message_form_builder.rb
+++ lib/validation_message_form_builder.rb
@@ -1,4 +1,10 @@
 class ValidationMessageFormBuilder < ActionView::Helpers::FormBuilder
+  def validation_message_id(field)
+    if errors(field).any?
+      field_id(field, :validation_message)
+    end
+  end
+
   def errors(field, &block)
     validation_messages = object.respond_to?(:errors) ? object.errors[field] : []
 
